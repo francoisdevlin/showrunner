4505bad16c32ea680b8e2c220f436b2cb9b384f5
Sean Devlin <francoisdevlin@gmail.com>
2016-09-28 13:43:16 -0400

    Added pausing for command completion, shorter delay after command finished, leading screenshot

--- applescript-writer.php
+++ applescript-writer.php
@@ -1,11 +1,20 @@
 <?
 $filename = $argv[1];
 $lines = preg_split(":\n:",trim(file_get_contents($filename)));
-//$lines = [
-	//"echo 'Bacon'",
-	//"pwd",
-	//"ls"
-	//];
+
+function snapshotGenerator($i){
+	$i = $i - 1;
+	return function() use(&$i){
+		$i++;
+		return <<<EOD
+	set shellCommand to "/usr/sbin/screencapture " & theDesktop & "Screen_Shot_$i.png"
+	do shell script shellCommand
+EOD;
+	};
+}
+
+$snapshot = snapshotGenerator(0);
+
 $snapshotBlock = <<<EOD
 	set theCurrentDate to (time of(current date))
 	set shellCommand to "/usr/sbin/screencapture " & theDesktop & "Screen_Shot_" & theCurrentDate & ".png"
@@ -16,6 +25,7 @@ $header = <<<EOD
 tell application "Terminal"
 	set theDesktop to POSIX path of (path to desktop as string)
 	activate
+	set frontWindow to window 1
 	set currentTab to do script "echo 'Hello World'"
 	tell application "System Events"
 		keystroke "+" using {command down}
@@ -36,8 +46,19 @@ tell application "Terminal"
 EOD;
 
 $output = $header;
+$output .= $snapshot() . "\n";
 foreach ($lines as $line) {
-	$output .= "\tdo script \"" . $line . "\" in currentTab\n\tdelay 5\n" . $snapshotBlock . "\n";
+	$output .= "\tset w to do script \"" . $line . "\" in currentTab\n".
+		<<<EOD
+	repeat 
+		delay 1
+		if not busy of w then exit repeat
+	end repeat
+
+EOD
+		. "\tdelay 1\n" 
+		. $snapshot() 
+		. "\n";
 }
 $output .= <<<EOD
 	delay 5
