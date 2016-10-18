ce7fe70ff4314b2e7a9f863bb8eb02bb17a42a81
Sean Devlin <francoisdevlin@gmail.com>
2016-10-18 12:01:35 -0400

    Basic split test

--- showrunner/file_split_test.go
+++ showrunner/file_split_test.go
@@ -23,3 +23,40 @@ Awesome?
 		t.Error("The output body does not match the expected value")
 	}
 }
+
+/*
+Tests the most basic split usage
+*/
+func TestBasicSplit(t *testing.T) {
+	comment := "--"
+	inputText := `Hi
+Everyone
+--split
+Isn't
+This
+--split
+Awesome?
+`
+	output := BuildText(comment, SplitLines(inputText))
+	if len(output) != 3 {
+		t.Error("The output array is the wrong size, it should be 3")
+	}
+	if output[0] != `Hi
+Everyone` {
+		t.Error("The first entry does not match the expected value")
+	}
+	if output[1] != `Hi
+Everyone
+Isn't
+This` {
+		t.Error("The second entry does not match the expected value")
+	}
+	if output[2] != `Hi
+Everyone
+Isn't
+This
+Awesome?
+` {
+		t.Error("The thrid entry does not match the expected value")
+	}
+}
