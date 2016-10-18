fbe8c5a422b612785b0f4cd3d8d0b3d285c977d9
Sean Devlin <francoisdevlin@gmail.com>
2016-10-18 15:58:30 -0400

    Added basic command line execution

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
 	"git.sevone.com/glider-guns/showrunner.git/showrunner"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	comment := "--"
-	inputText := `Hi
---split
-Everyone
-Isn't
---split
-This
---re-split
-Terrible?
---split
-Awesome?
-`
+	wordPtr := flag.String("file", "foo", "This is a file to split up")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "This is a tool to assist with the creation of training material")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	fmt.Println(*wordPtr)
+	fmt.Println(filepath.Base(*wordPtr))
+	fmt.Println(filepath.Dir(*wordPtr))
+	fmt.Println(filepath.Ext(*wordPtr))
+	comment := showrunner.GetLineCommentString(*wordPtr)
+
+	buff, _ := ioutil.ReadFile(*wordPtr)
+	inputText := string(buff)
 	for i, temp := range showrunner.BuildText(comment, showrunner.SplitLines(inputText)) {
-		fmt.Println(i)
+		fmt.Println(i, "\n")
 		fmt.Println(temp)
 	}
+
 }
