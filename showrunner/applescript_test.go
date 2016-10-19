00a3fd7e4fd647ef2b85bea048ea6685cb1a49cf
Sean Devlin <francoisdevlin@gmail.com>
2016-10-19 16:25:16 -0400

    Added test case for long words

--- showrunner/applescript_test.go
+++ showrunner/applescript_test.go
@@ -22,6 +22,23 @@ func TestBasicWord(t *testing.T) {
 		delay .02
 	end tell
 `},
+		[]string{"baconmaster", `	tell application "System Events"
+		keystroke "b"
+		delay .02
+		keystroke "a"
+		delay .02
+		keystroke "c"
+		delay .02
+		keystroke "o"
+		keystroke "n"
+		keystroke "m"
+		keystroke "a"
+		keystroke "s"
+		keystroke "t"
+		keystroke "e"
+		keystroke "r"
+	end tell
+`},
 	}
 	for _, pair := range pairs {
 		input := pair[0]
