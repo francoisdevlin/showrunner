22a5c1ba593cf00abc2f50a0393166901e490426
Sean Devlin <francoisdevlin@gmail.com>
2016-10-19 22:04:43 -0400

    The applescript is automatically started

--- showrunner/applescript_command.go
+++ showrunner/applescript_command.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	//"path/filepath"
+	"os/exec"
 )
 
 type Applescript struct {
@@ -14,6 +14,7 @@ type Applescript struct {
 func (this *Applescript) Main(args []string) error {
 	f := flag.NewFlagSet("test", flag.ExitOnError)
 	wordPtr := f.String("file", "foo", "This is a file to split up")
+	outfile := f.String("intermediate-file", "/tmp/temp_command.applescript", "This is the intermediate file that stores the applescript")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "This is a tool to assist with the creation of training material")
 		flag.PrintDefaults()
@@ -23,6 +24,15 @@ func (this *Applescript) Main(args []string) error {
 	buff, _ := ioutil.ReadFile(*wordPtr)
 	inputText := string(buff)
 	lines := SplitLines(inputText)
-	fmt.Println(lineBuilder(lines, *wordPtr, 13))
+	fileContents := lineBuilder(lines, *wordPtr, 13)
+	err := ioutil.WriteFile(*outfile, []byte(fileContents), 0644)
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command("/usr/bin/osascript", *outfile)
+	_, execErr := cmd.Output()
+	if execErr != nil {
+		return execErr
+	}
 	return nil
 }
